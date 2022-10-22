22d4d71abf2c243e6faa37d0494fdf7ee5eb84e9
lindisjay <lindisrijayanti3771@gmail.com>
2022-10-22 11:51:46 +0700

    menambahkan list ke stores dan services todo.js

--- src/stores/dashboard/todo.js
+++ src/stores/dashboard/todo.js
@@ -1,25 +1,34 @@
 import { defineStore } from "pinia";
 
-import * as s$todo from '@/services/dashboard/todo';
+import * as s$todo from "@/services/dashboard/todo";
 
 const d$todo = defineStore({
-    id: 'todo',
-    state: () => ({
-        list: [],
-    }),
-    actions: {
-        async a$add(body) {
-            try {
-              await s$todo.add(body);
-            } catch (e) {
-              console.error('actions todo add error', e);
-              throw e;
-            }
-        },
+  id: "todo",
+  state: () => ({
+    list: [],
+  }),
+  actions: {
+    async a$list() {
+      try {
+        const { data } = await s$todo.list();
+        this.list = data;
+      } catch (e) {
+        console.error("actions todo list error", e);
+        throw e;
+      }
     },
-    getters: {
-        
+    async a$add(body) {
+      try {
+        await s$todo.add(body);
+      } catch (e) {
+        console.error("actions todo add error", e);
+        throw e;
+      }
     },
+  },
+  getters: {
+    g$list: ({ list }) => list,
+  },
 });
 
 export default d$todo;
